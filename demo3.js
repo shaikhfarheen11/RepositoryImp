0a4df143f8c12fb612042457c2f031d42702ddd1
shaikhfarheen11 <farheens554>
2023-02-23 20:31:28 +0530

    first commit

---
+++ demo3.js
@@ -0,0 +1,23 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+    items[0].style.backgroundColor = 'pink';
+    items[1].style.backgroundColor = 'skyblue';
+    items[2].style.backgroundColor = 'yellow';
+    items[3].style.backgroundColor = 'lightgreen';
+
+
+}
+var li = getElementsByTagName('li');
+console.log(li);
+li[i].style.fontWeight = 'bold'; 
+li[i].style.backgroundColor = 'yellow';
+
+
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+
