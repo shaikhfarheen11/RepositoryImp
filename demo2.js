c356950fd067eda6d49c2a833c1517ae098d73f9
shaikhfarheen11 <farheens554>
2023-02-23 14:31:50 +0530

    first commit

---
+++ demo2.js
@@ -0,0 +1,10 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor = 'green';
+items[0].style.fontWeight = 'bold';
+items[1].style.fontWeight = 'bold';
+items[2].style.fontWeight = 'bold';
+items[3].style.fontWeight = 'bold';
+
+
